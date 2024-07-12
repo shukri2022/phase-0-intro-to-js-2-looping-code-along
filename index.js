060a4d1247237ac1c25954e058315db2bc99bdfc
Shukri Abdi <shukri751@gmail.com>
2024-07-12 21:55:30 +0300

    loop lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+  }
+
+  return messages;
+}
+
+
+// countdown
+
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+      console.log(i);
+  }
+}
+
+
+
